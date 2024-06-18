773e377ae6a60a5a671bc7137459dfe06b2aa5ab
miftakhulagustina <miftakhul804@gmail.com>
2024-06-19 06:27:31 +0700

    handle negatif input

--- bank-account.js
+++ bank-account.js
@@ -1,13 +1,23 @@
 //Fungsi menambah saldo
-let tambahSaldo = () => saldo + +amount;
+let tambahSaldo = (amount) => {
+    if (amount >= 0) {
+        tambah = saldo + +amount;
+        return "Saldo saat ini " + tambah;
+    } else {
+        return "Inputan tidak sesuai!";
+    }
+}
 
 //Fungsi mengurangi saldo
-let kurangiSaldo = () => {
-    if (amount <= saldo) {
-        return saldo - amount;
+let kurangiSaldo = (amount) => {
+    if (amount <= saldo && amount >= 0) {
+        kurang = saldo - amount;
+        return "Saldo saat ini " + kurang;
     }
-    else {
-        return "tidak mencukupi untuk melakukan transaksi";
+    else if (amount <= 0) {
+        return "Inputan tidak sesuai!";
+    } else {
+        return "Saldo tidak mencukupi untuk melakukan transaksi";
     }
 }
 
@@ -15,9 +25,9 @@ let saldo = 250000;
 let amount = window.prompt("Silahkan masukkan nominal! \nSaldo Anda saat ini adalah " + saldo);
 let pilih = window.prompt("Silahkan pilih transaksi! \n 1. tambah saldo \n 2. kurangi saldo");
 if (pilih == 1) {
-    console.log("Saldo Anda saat ini " + tambahSaldo());
+    console.log(tambahSaldo(amount));
 } else if (pilih == 2) {
-    console.log("Saldo Anda saat ini " + kurangiSaldo());
+    console.log(kurangiSaldo(amount));
 } else {
     console.log("Input tidak sesuai! \nSilahkan input (1) untuk menambah saldo atau (2) untuk mengurangi saldo");
 }
